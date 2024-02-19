cc4de6e6df579729ccf4daa5991a5c94d8cfaf6c
DESKTOP-IFMN175\804-03 <mangoperry2015@gmail.com>
2024-02-19 10:53:54 +0900

    Initial commit

---
+++ src/main/java/org/ohgiraffers/board/controller/PostController.java
@@ -0,0 +1,4 @@
+package org.ohgiraffers.board.controller;
+
+public class PostController {
+}
---
+++ src/main/java/org/ohgiraffers/board/repository/PostRepository.java
@@ -0,0 +1,7 @@
+package org.ohgiraffers.board.repository;
+
+import org.ohgiraffers.board.domain.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository {
+}
