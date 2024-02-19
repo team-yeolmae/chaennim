bfccd75aa57ec652778f6f37b568b81f029e111b
DESKTOP-IFMN175\804-03 <mangoperry2015@gmail.com>
2024-02-19 11:11:58 +0900

    board structure

--- src/main/java/org/ohgiraffers/board/domain/entity/Post.java
+++ src/main/java/org/ohgiraffers/board/domain/entity/Post.java
@@ -1,4 +1,23 @@
 package org.ohgiraffers.board.domain.entity;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Post {
+
+    @Id
+    @GeneratedValue
+    private Long postId;
+
+    private String title;
+    private String content;
+
 }
